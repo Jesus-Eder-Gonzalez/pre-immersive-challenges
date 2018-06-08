3c6f26cb294fcf14de31652f97e8067c4d6af42c
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-06-07 16:36:33 -1000

    Solution for check ascending order

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,38 @@
 module.exports = function acceptableSequence(str) {
-  // write your code here
+  if (str.length > 0) {
+    var letters =  str.match(/[A-z]/gi);
+    var exact = str.match(/\+[A-Z]\+/gi);
+
+    if (exact != undefined) {
+      if (exact.length == letters.length) {
+        return true;
+      }
+      else {
+        var edger = str.substring(0,2).match(/[A-Z]\+/gi);
+        var edgel = str.substring(str.length-2,str.length).match(/\+[A-Z]/gi);
+        var edge = 0;
+
+        if (edger != undefined) {
+          edge = edge + 1;
+        }
+
+        if (edgel != undefined) {
+          edge = edge + 1;
+        }
+
+        if ((letters.length + edge) == exact.length){
+          return true;
+        }
+        else {
+          return false;
+        }
+      }
+    }
+    else {
+      return false;
+    }
+  }
+  else {
+    return false;
+  }
 }
--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,21 @@
 module.exports = function checkAscOrder(numArray) {
-  // write your code in here
+//  var length = numArray.length;
+//  tempArray = numArray.slice(1,length);
+//  console.log(tempArray);
+//  for (i = 0; i < length-1; i++) {
+//   console.log(numArray[i]);
+//   if (tempArray.every(x => x < numArray[i])){
+//     return false;
+//   }
+//   tempArray = numArray.slice(i+1,length);
+//   console.log(tempArray);
+//  }
+//  return true;
+var length = numArray.length;
+for (i = 0; i < length - 1; i++) {
+  if(numArray[i]>numArray[i+1]){
+    return false;
+  }
+}
+return true;
 }
