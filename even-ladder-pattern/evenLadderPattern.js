bc481bb940c4ca39a71634fc927ad9f495aa20e2
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-06-07 19:12:19 -1000

    Added solution for even ladder pattern.

--- even-ladder-pattern/evenLadderPattern.js
+++ even-ladder-pattern/evenLadderPattern.js
@@ -1,3 +1,32 @@
 module.exports = function evenLadderPattern(num) {
-  // write your code in here
+  var ladder = "";
+  if (num > 1){
+    if(num%2 === 0) {
+      for(i=2; i <=num; i+=2){
+        //console.log(i);
+        for(j=0; j < i; j++){
+          ladder = ladder.concat(i);
+        }
+        if(i+2 <= num){
+          ladder = ladder.concat('\n');
+        }
+      }
+    }
+    else{
+
+      for(i=2; i <num; i+=2){
+        //console.log(i);
+        for(j=0; j < i; j++){
+          ladder = ladder.concat(i);
+        }
+        if(i+2 < num){
+          ladder = ladder.concat('\n');
+        }
+      }
+    }
+    return ladder;
+  }
+  else {
+    return "";
+  }
 }
