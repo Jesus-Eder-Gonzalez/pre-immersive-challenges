0af0deaeac6a097fd96bed6ad4ea39e196b9db56
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-06-07 18:26:02 -1000

    Added solution to this challenge.

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,3 +1,25 @@
 module.exports = function descendingOrder(number) {
-  // write code in here
+  //console.log(typeof number);
+  var strNum;
+  var newNum = [];
+  if (typeof number !== "number"){
+    return 'not a number!';
+  }
+  else {
+    strNum = number.toString();
+    //console.log(strNum);
+    for (i = 0; i < strNum.length; i++){
+      newNum.push(strNum[i]);
+    }
+    //console.log(newNum);
+    newNum = newNum.sort().reverse();
+    //console.log(newNum);
+    strNum = "";
+    for (i = 0; i < newNum.length; i++){
+      strNum = strNum.concat(newNum[i]);
+    }
+    //console.log(strNum);
+    return Number(strNum);
+
+  }
 }
