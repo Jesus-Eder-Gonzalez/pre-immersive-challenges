8c7674c5102a38fee0ae352dd1b083e823d9fc74
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-06-07 16:58:27 -1000

    Added my solution

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,18 @@
 module.exports = function cookieProblem(array) {
-  // write your code in here
+  var max = 0;
+  var cookies = 0;
+  for (i = 0; i < array.length; i++){
+    if (array[i] > max){
+      max = array[i];
+      // console.log(max);
+    }
+  }
+  // console.log("cookies");
+  for (i = 0; i <array.length; i++){
+    if (array[i] !== max) {
+      cookies = cookies + (max-array[i]);
+      // console.log(cookies);
+    }
+  }
+  return cookies;
 }
