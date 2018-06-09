968c6bf425a2e678e62bdd2471be36d41e96604e
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-06-08 21:53:35 -1000

    Added my solution for the find-unique-num challenge.

--- find-unique-num/findUniqueNum.js
+++ find-unique-num/findUniqueNum.js
@@ -1,3 +1,22 @@
 module.exports = function findUniqueNum(strOfNums) {
-  // write your code in here
+  var even;
+  var odd;
+  var eCount = 0;
+  var tempArray = strOfNums.split(" ");
+  for(i = 0; i < tempArray.length; i++){
+    //console.log(tempArray[i] + " " + tempArray[i]%2);
+    if(tempArray[i]%2 === 0) {
+      //console.log("index "+i + " even");
+      even = i;
+      eCount += 1;
+    }
+    else {
+      odd = i;
+    }
+  }
+  if (eCount > 1) {
+    return odd;
+  } else {
+    return even;
+  }
 }
