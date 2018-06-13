0ba3b53844a8d7b26e36ed642efe5a401a4211ca
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-06-12 17:17:44 -1000

    Added my solution for insertDashes challenge.

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,10 @@
 module.exports = function insertDashes(num) {
-  // write your code in here
+  var str = num.toString();
+  if (str.match(/[02468]/g) !== null){
+    var firstRun = str.replace(/[02468][02468]/g,x => x.charAt(0)+"-" +x.charAt(1));
+    return firstRun.replace(/[02468][02468]/g,x => x.charAt(0)+"-" +x.charAt(1));
+  }
+  else {
+    return str;
+  }
 }
