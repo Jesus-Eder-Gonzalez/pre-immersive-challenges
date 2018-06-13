a87b8169b28ab3d2625bc2893361927ceefa944b
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-06-12 19:16:44 -1000

    Added solution for swap letter case challenge.

--- swap-letter-case/swapLetterCase.js
+++ swap-letter-case/swapLetterCase.js
@@ -1,3 +1,17 @@
 module.exports = function swapLetterCase(str) {
-  // write your code in here
+  var process = str.split("");
+  function swapCase(x){
+    var unicode = x.charCodeAt();
+    if (65 <= unicode && unicode < 91){
+      return String.fromCharCode(unicode+32);
+
+    } else if (97 <= unicode && unicode <123){
+      return String.fromCharCode(unicode-32);
+    }else {
+      return String.fromCharCode(unicode);
+    }
+
+
+  }
+  return process.map(x => swapCase(x)).join("") ;
 }
