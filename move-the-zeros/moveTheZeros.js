71d8ad2873d5497f42a9b35ee8d55bcab0582b5d
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-06-12 17:39:52 -1000

    Added solution to moveTheZeroes challenge.

--- move-the-zeros/moveTheZeros.js
+++ move-the-zeros/moveTheZeros.js
@@ -1,3 +1,15 @@
 module.exports = function moveTheZeros(arrNum, isRight) {
-  // write your code here
+  //console.log(arrNum, isRight);
+  var tempArr;
+  if(arrNum !== null){
+    if(isRight === true){
+      tempArr = arrNum.filter(x => x !== 0);
+      tempArr = tempArr.concat(arrNum.filter(x => x === 0));
+    }
+    else {
+      tempArr = arrNum.filter(x => x === 0);
+      tempArr = tempArr.concat(arrNum.filter(x => x !== 0));
+    }
+  }
+  return tempArr;
 }
